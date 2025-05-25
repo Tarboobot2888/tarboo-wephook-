10d17f884082e2c8de50ce6b21bd9a8d797132d4
Tarboobot2888 <129686276+Tarboobot2888@users.noreply.github.com>
2025-05-25 06:58:45 +0300

    Update cli-server.ts

--- lib/webcrack/cli-server.ts
+++ lib/webcrack/cli-server.ts
@@ -2,23 +2,32 @@ import { existsSync, readFileSync } from 'fs';
 import { readFile, rm } from 'fs/promises';
 import path from 'path';
 import { Command } from 'commander';
+import { parse } from '@babel/parser';
+import generate from '@babel/generator';
 import deobfuscate from './index';
+import { saveOnServer } from './index';
 
 const program = new Command();
 
 program
   .name('webcrack')
   .description('Deobfuscate JavaScript using WebCrack')
-  .version('0.0.1')
+  .version('1.0.0')
   .argument('<file>', 'Input JavaScript file')
   .option('-o, --output <dir>', 'Output directory')
   .action(async (file, options) => {
     const input = await readFile(file, 'utf8');
-    const ast = deobfuscate.parse(input);
-    await deobfuscate.run(ast);
-    const code = deobfuscate.generate(ast);
+
+    const ast = parse(input, {
+      sourceType: 'unambiguous',
+      plugins: ['jsx', 'typescript'],
+    });
+
+    await deobfuscate.run(ast, { changes: 0 }, undefined);
+    const { code } = generate(ast, { comments: false });
+
     if (options.output) {
-      await deobfuscate.saveOnServer(options.output, code);
+      await saveOnServer(options.output, code);
     } else {
       console.log(code);
     }
